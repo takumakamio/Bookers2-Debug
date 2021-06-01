f05de96716eb80720757c1204a877120de086e44
EC2 Default User <ec2-user@ip-172-31-19-107.ec2.internal>
2021-06-01 16:48:12 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @booknew = Book.new
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def create
+    @user = current_user
+    @books =Book.all
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice]="Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render 'index'
+    end
+  end
+
+   def edit
+    @book = Book.find(params[:id])
+    if @book.user.id != current_user.id
+    redirect_to books_path
+    end
+   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+    
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
